5f6444beda0bd644156e72963ec17a1d18ffa7c0
SamiSTR <117772806+SamiSTR@users.noreply.github.com>
2025-03-20 14:15:13 +0300

    Update islower().py

--- Samuel Tesfaye/String Method/islower()/islower().py
+++ Samuel Tesfaye/String Method/islower()/islower().py
@@ -1,1 +1,12 @@
 
+S = "samket"
+print(S.islower())  # Output: True
+
+R = "Samket"
+print(R.islower())  # Output: False
+
+T = "samketredu"
+print(T.islower())  # Output: True
+
+empty_string = ""
+print(empty_string.islower())  # Output: False
