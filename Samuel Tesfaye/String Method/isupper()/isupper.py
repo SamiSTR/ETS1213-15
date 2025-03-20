99afe24b7dcb389fa1b87dea10af02815d862956
SamiSTR <117772806+SamiSTR@users.noreply.github.com>
2025-03-20 14:08:08 +0300

    Update isupper.py

--- Samuel Tesfaye/String Method/isupper()/isupper.py
+++ Samuel Tesfaye/String Method/isupper()/isupper.py
@@ -1,1 +1,11 @@
+X = "HALF"
+print(X.isupper()))  # Output: True
 
+Y = "half"
+print(Y.isupper())  # Output: False
+
+Z = "HALFLIFE"
+print(Z.isupper())  # Output: True
+
+empty_string = ""
+print(empty_string.isupper())  # Output: False
